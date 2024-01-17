3de9ca1c84c13c6d81d7b5ce1ea6f399769c33f9
Lasse Haverinen <lasse.haverinen@gmail.com>
2024-01-17 10:54:50 +0200

    Added exercise 7. half ready

---
+++ lesson3/exercise7.js
@@ -0,0 +1,80 @@
+let sweden = {
+  name: "Sweden",
+  capital: "Stockholm",
+  languages: ["Swedish"],
+  area: 450295,
+  population: 10302731,
+  gdpPerCapita: 50579,
+  iso3166Code: "SE",
+  largestCities: [
+    { name: "Stockholm", population: 1515017 },
+    { name: "Gothenburg", population: 572799 },
+    { name: "Malmö", population: 312012 },
+    { name: "Uppsala", population: 140454 },
+    { name: "Västerås", population: 110877 },
+  ],
+};
+
+// Initialize france object
+let france = {
+  name: "France",
+  capital: "Paris",
+  languages: ["French"],
+  area: 640679,
+  population: 67158000,
+  gdpPerCapita: 43551,
+  iso3166Code: "FR",
+  largestCities: [
+    { name: "Paris", population: 2140526 },
+    { name: "Marseille", population: 855393 },
+    { name: "Lyon", population: 513275 },
+    { name: "Toulouse", population: 479553 },
+    { name: "Nice", population: 340017 },
+  ],
+};
+
+// Initialize china object
+let china = {
+  name: "China",
+  capital: "Beijing",
+  languages: ["Chinese"],
+  area: 9596960,
+  population: 1403500365,
+  gdpPerCapita: 16624,
+  iso3166Code: "CN",
+  largestCities: [
+    { name: "Shanghai", population: 24183300 },
+    { name: "Beijing", population: 20794100 },
+    { name: "Chongqing", population: 15376000 },
+    { name: "Tianjin", population: 12938224 },
+    { name: "Guangzhou", population: 12702800 },
+  ],
+};
+
+let arrayOfCountries = [france, sweden, china];
+
+/* Print out for each country in countries array:
+    the name of the country,  
+    the total population of the country,  
+    the names of the five biggest cities in the country and their population , 
+    the total sum of population in the five biggest cities  
+    the percentage of the population in those cities out of the total population in the country.
+    */
+function printCountryData(countries) {
+  countries.forEach((currentCountry) => {
+    console.log("Country name: " + currentCountry.name);
+    console.log("Population: " + currentCountry.population);
+    console.log("Names of five biggest cities and population: ");
+    currentCountry.largestCities.forEach((currentCity, index) => {
+      console.log(
+        `${index + 1}. ${currentCity.name}, population is: ${
+          currentCity.population
+        }`
+      );
+    });
+
+    console.log("\n");
+  });
+}
+
+printCountryData(arrayOfCountries);
