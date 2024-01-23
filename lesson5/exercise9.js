f9eab6c86dbbe594af534fedb98526c83c3198f3
Lasse Haverinen <lasse.haverinen@oamk.fi>
2024-01-23 11:31:26 +0200

    Added teacher example to exercise9

--- lesson5/exercise9.js
+++ lesson5/exercise9.js
@@ -5,15 +5,26 @@ function Student(name, countryCodeForBirthplace) {
   // for example random number between 0 - 10000000
   this.studentId = Math.floor(Math.random() * 10000000);
 
-  
-
-
 }
 
-let student1 = new Student("Lasse", "FI");
+function Teacher(name, countryCodeForBirthplace) {
+  this.name = name;
+  this.birthplaceCountryCode = countryCodeForBirthplace;
+  this.teachingTopics = [];
+
+  this.addTeachingTopic = function(teachingTopic) {
+    this.teachingTopics.push(teachingTopic); 
+   };
+}
+
+let student1 = new Student("Tommi", "FI");
 let student2 = new Student("John", "US");
 let student3 = new Student("Mary", "UK");
 
-console.log(student1);
-console.log(student2);
-console.log(student3);
+let teacher1 = new Teacher("Lasse", "FI");
+console.log(teacher1);
+teacher1.addTeachingTopic("JavaScript");
+// teacher1.addTeachingTopic("Python");
+
+console.log(teacher1);
+
