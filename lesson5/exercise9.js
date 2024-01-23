403b3d269846310b000d12ff75d149d1d43e20b3
Lasse Haverinen <lasse.haverinen@oamk.fi>
2024-01-23 11:46:28 +0200

    Exercise 9 finishing changes for lesson5

--- lesson5/exercise9.js
+++ lesson5/exercise9.js
@@ -5,6 +5,10 @@ function Student(name, countryCodeForBirthplace) {
   // for example random number between 0 - 10000000
   this.studentId = Math.floor(Math.random() * 10000000);
 
+  this.tutor = undefined;
+  this.setTutor = function(teacherTutor) {
+    this.tutor = teacherTutor;
+  };
 }
 
 function Teacher(name, countryCodeForBirthplace) {
@@ -17,6 +21,16 @@ function Teacher(name, countryCodeForBirthplace) {
    };
 }
 
+/* Your task here is to add a tutor property to the Student constructor function
+   so that all students have a tutor property.
+
+   You should add a setTutor method to the Student constructor function so that
+   the tutor property can be set.
+
+   Add teacher1 as a tutor to the student1 object.
+   */
+
+
 let student1 = new Student("Tommi", "FI");
 let student2 = new Student("John", "US");
 let student3 = new Student("Mary", "UK");
@@ -24,7 +38,12 @@ let student3 = new Student("Mary", "UK");
 let teacher1 = new Teacher("Lasse", "FI");
 console.log(teacher1);
 teacher1.addTeachingTopic("JavaScript");
-// teacher1.addTeachingTopic("Python");
-
 console.log(teacher1);
+
+console.log(student1);
+student1.setTutor(teacher1);
+console.log(student1);
+console.log('Changing teacher1 name');
+teacher1.name = "Some other name";
+console.log(student1);
 
