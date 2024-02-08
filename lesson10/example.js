b22c5e9a51334d37497a5c34933728aa66cfe249
Lasse Haverinen <lasse.haverinen@oamk.fi>
2024-02-08 14:06:18 +0200

    Lesson 10 content added

---
+++ lesson10/example.js
@@ -0,0 +1,8 @@
+console.log('Hello from example.js!');
+
+let element = document.getElementById('test');
+
+console.log(element);
+
+console.log('Text value of the element is');
+console.log(element.innerText);
