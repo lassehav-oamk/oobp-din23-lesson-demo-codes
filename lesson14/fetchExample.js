e6ee333e16289d5c5ba526e05cd90c25b3530600
Lasse Haverinen <lasse.haverinen@oamk.fi>
2024-02-12 09:54:27 +0200

    Fetch example finished

--- lesson14/fetchExample.js
+++ lesson14/fetchExample.js
@@ -4,28 +4,32 @@ function getAndCreateProductElements() {
   // step 0 add this function to the event listener of the button
 
   // step 1 send fetch request to dummyJSON
-
+  fetch('https://dummyjson.com/products')
   // step 2 receive the response and convert it to JSON
+  .then(response => response.json())
+  .then(responseData => {
+    console.log(responseData);
+    // step 3 create the product list with the code below adapted to the JSON data
+    // create product list div
+    const productList = document.createElement('div');
+    productList.className = 'productList';
 
-  // step 3 create the product list with the code below adapted to the JSON data
+    for(let i = 0; i < responseData.products.length; i++) {
+      let productCard = createProductCard(
+        responseData.products[i].title,
+        responseData.products[i].thumbnail,
+        responseData.products[i].description,
+        responseData.products[i].price + "€");
 
-  // create product list div
-  // const productList = document.createElement('div');
-  // productList.className = 'productList';
+      // add the product card to the product list
+      productList.appendChild(productCard);
+    }
 
-  // for(let i = 0; i < cameraData.length; i++) {
-  //   let productCard = createProductCard(
-  //     cameraData[i].model,
-  //     cameraData[i].image,
-  //     cameraData[i].highlights[0] + ', ' + cameraData[i].highlights[1] + ', ' + cameraData[i].highlights[2],
-  //     cameraData[i].highlights[3] + ', ' + cameraData[i].highlights[4]);
+    //add the product list to the body
+    document.querySelector('div#productDataTarget').appendChild(productList);
+  })
 
-  //   // add the product card to the product list
-  //   productList.appendChild(productCard);
-  // }   
   
-  // // add the product list to the body
-  // document.querySelector('body').appendChild(productList);
 }
 
 function createProductCard(productName, productImage, productDescription1, productDescription2){        
